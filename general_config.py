eb2ec8589b50e26b42f31477ce3db03042be8ab1
Baher Mohammad <bahermh19@gmail.com>
2025-11-03 14:19:55 +0300

    update data path

--- general_config.py
+++ general_config.py
@@ -1,2 +1,2 @@
-images_path = "C:/Users/baher/OneDrive/Desktop/masters/masters thesis/master_code/data"
+images_path = "C:/Users/baher/OneDrive/Desktop/masters/masters thesis/data"
 output_path = "C:/Users/baher/OneDrive/Desktop/masters/masters thesis/master_code/outputs"
