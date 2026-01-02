57f4e05ed36a711e28854132b381a3293289558b
Baher Mohammad <bahermh19@gmail.com>
2026-01-02 12:42:55 +0300

    add a new model and a new dataset

--- config.py
+++ config.py
@@ -7,13 +7,15 @@ from src.methods.model_tesseract import ModelTesseract
 dataset_paths = {
     "RusTwit": "../data/RusTwit.tsv",
     "RusTwit_real": "../data/RusTwit_real.tsv",
-    "OCRBench": "../data/OCRBench.tsv"
+    "OCRBench": "../data/OCRBench.tsv",
+    "Simple": "../data/Simple.tsv"
 }
 
 # Store classes, not instances
 models = {
     "ppocr_v5_mobile_ru": partial(ModelPaddleOCR, model_name="ppocr_v5_mobile_ru"),
-    "ppocr_v5_mobile_ru_finetuned": partial(ModelPaddleOCR, model_name="ppocr_v5_mobile_ru_finetuned", rec_model_dir="C:/Users/baher/OneDrive/Desktop/masters/masters_thesis/paddleOCR/PaddleOCR/inference/eslav_rec_ppocr_v5"),
+    "ppocr_v5_mobile_ru_finetuned": partial(ModelPaddleOCR, model_name="ppocr_v5_mobile_ru_finetuned", rec_model_dir="C:/Users/baher/OneDrive/Desktop/masters/masters_thesis/paddleOCR/PaddleOCR/inference/eslav_rec_ppocr_v5_exp1"),
+    "ppocr_v5_mobile_ru_finetuned_exp2": partial(ModelPaddleOCR, model_name="ppocr_v5_mobile_ru_finetuned_exp2", rec_model_dir="C:/Users/baher/OneDrive/Desktop/masters/masters_thesis/paddleOCR/PaddleOCR/inference/eslav_rec_ppocr_v5_exp2"),
     "ppocr_v5_server": partial(ModelPaddleOCR, model_name = "PP-OCRv5_server_rec"),
     "EasyOCR": ModelEasyOCR,
     "EasyOCR_en": partial(ModelEasyOCR, lang='en'),
--- general_config.py
+++ general_config.py
@@ -1,2 +1,2 @@
-images_path = "C:/Users/baher/OneDrive/Desktop/masters/masters_thesis/data"
-output_path = "C:/Users/baher/OneDrive/Desktop/masters/masters_thesis/master_code/outputs"
+images_path = "../data"
+output_path = "./outputs"
